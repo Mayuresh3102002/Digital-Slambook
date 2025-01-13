6a5d2d6f45386cac98f9f9363a2de6da70e8030a
Mayuresh3102002 <mayureshgajalwar3@gmail.com>
2025-01-14 01:08:00 +0530

    commit-mayuresh-2

--- src/main/java/com/project/digitalslambook/repository/AdminRepository.java
+++ src/main/java/com/project/digitalslambook/repository/AdminRepository.java
@@ -1,9 +1,15 @@
 package com.project.digitalslambook.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.project.digitalslambook.entity.Admin;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
-
+	
+	List<Admin> findByAge(int byage);
+	
 }
--- src/main/java/com/project/digitalslambook/service/AdminService.java
+++ src/main/java/com/project/digitalslambook/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.project.digitalslambook.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,13 +14,36 @@ public class AdminService {
 
 	@Autowired
 	private AdminRepository adminRepository;
-	
+
 	public Admin addAdmin(Admin admin) {
 		return adminRepository.save(admin);
 	}
 
 	public List<Admin> findAllAdmins() {
 		return adminRepository.findAll();
+	}
+
+	public List<Admin> findAdminByAge(int byage) {
+		return adminRepository.findByAge(byage);
+	}
+
+	public Admin updateAdmin(Admin admin) {
+		admin.setName(admin.getName());
+		admin.setMobile(admin.getMobile());
+		admin.setAbout(admin.getAbout());
+		admin.setAge(admin.getAge());
+		admin.setAmbition(admin.getAmbition());
+		admin.setApprating(admin.getApprating());
+		
+		return adminRepository.save(admin);
+	}
+
+	public Admin deleteAdmin(int id) {
+		Optional<Admin> admin = adminRepository.findById(id);
+		if(admin != null) {
+			adminRepository.deleteById(id);
+		}
+		return null;
 	}
 
 }
